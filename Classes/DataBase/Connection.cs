028859ff2a0ed812527e115631a9ca982005a21e
Kolovratik <Kolovratik@HOME-PC>
2024-06-13 19:41:30 +0300

    Реализован класс подключения к базе данных и класс общего ползования

---
+++ Classes/DataBase/Connection.cs
@@ -0,0 +1,29 @@
+﻿using System.Data.SqlClient;
+
+namespace PR43.Classes.DataBase
+{
+    public class Connection
+    {
+        private static readonly string config = "server=HOME-PC\\MYSERVER;" +
+            "Trusted_Connection=No;" +
+            "DataBase=BooksAndAuthors;" +
+            "User=sa;" +
+            "PWD=sa";
+        public static SqlConnection OpenConnection()
+        {
+            SqlConnection connection = new SqlConnection(config);
+            connection.Open();
+            return connection;
+        }
+        public static SqlDataReader Query(string sql, out SqlConnection connection)
+        {
+            connection = OpenConnection();
+            return new SqlCommand(sql, connection).ExecuteReader();
+        }
+        public static void CloseConnection(SqlConnection connection)
+        {
+            connection.Close();
+            SqlConnection.ClearPool(connection);
+        }
+    }
+}
